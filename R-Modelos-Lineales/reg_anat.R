4e23a65ae3c5708801b171e7ffe9c3a811131b21
Mario A. Garcia-Meza <marius.agm@gmail.com>
2022-06-03 20:01:48 -0500

    Creamos una base de datos de carros

---
+++ R-Modelos-Lineales/reg_anat.R
@@ -0,0 +1,5 @@
+# 'https://raw.github.com/scunning1975/mixtape/master/auto.dta'
+
+#df <- haven::read_dta('https://raw.github.com/scunning1975/mixtape/master/auto.dta')
+df <- read.csv("../data/cars.csv")
+
