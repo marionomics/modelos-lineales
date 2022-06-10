8bdcc2de66adea6c278215d8cb49c0ed717cb42a
Mario A. Garcia-Meza <marius.agm@gmail.com>
2022-06-10 18:33:09 -0500

    Hicimos otra simulacion

--- R-Modelos-Lineales/Cluster_SE/cluster1.R
+++ R-Modelos-Lineales/Cluster_SE/cluster1.R
@@ -1,5 +1,6 @@
 # Libraries
 library(mvtnorm)
+library(multiwayvcov)
 
 create_cluster <- function(rho = 0.5, n = 1000,
 n_cluster = 50, param = c(0.1, 0.5)){
@@ -21,5 +22,24 @@ n_cluster = 50, param = c(0.1, 0.5)){
     return(df)
 }
 
+# Simulate clustered dataset and fit with OLS
+cluster_sim <- function(param = c(0.1, 0.5), cluster_robust = FALSE){
+    df <- create_cluster(param = param)
+    model <- lm(y ~ x, data = df)
+    b1 <- coef(model)[2]
+    if(!cluster_robust){
+        Sigma <- vcov(model)
+        se <- sqrt(diag(Sigma)[2])
+        b1_ci95 <- confint(model)[2,]
+    }else{
+        Sigma = cluster.vcov(model, ~ cluster)
+        se = sqrt(diag(Sigma)[2])
+        t_critical <- qt(0.025, df = n -2, lower.tail = FALSE)
+        lower  <- b1 - t_critical*se
+        upper <- b1 + t_critical * se
+        b1_ci95 <- c(lower, upper)
+    }
+    return(c(b1, se, b1_ci95))
+}
+cluster_sim(cluster_robust = FALSE)
 
-create_cluster()
