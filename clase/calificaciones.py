8598ff8005ae3e3fed73a93fc660dcca4e7124d5
Mario A. Garcia-Meza <marius.agm@gmail.com>
2022-06-30 13:10:49 -0500

    subimos calificaciones y un script que las calcula en python

---
+++ clase/calificaciones.py
@@ -0,0 +1,13 @@
+def run():
+    from pandas import read_csv
+
+    df = read_csv("clase/grades.csv")
+    print(df.name)
+    i = input("Hola! Para saber tu calificación escribe el número en el que apareces en la lista de arriba: ")
+    print("Hola " + str(df.iloc[int(i),0]) +"!")
+    calificacion = df.iloc[int(i),1]*0.3 + df.iloc[int(i),2]*0.3 + df.iloc[int(i),3]*0.1 + df.iloc[int(i),4]*0.3
+    print("Tu calificación es " + str(calificacion))
+    
+
+if __name__ == "__main__":
+    run()
