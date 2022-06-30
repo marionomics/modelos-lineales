f3b03fe51cf526522628b0308fb2720d5cbac8d4
Mario A. Garcia-Meza <marius.agm@gmail.com>
2022-06-30 13:12:40 -0500

    minor udate to calculate grades

--- clase/calificaciones.py
+++ clase/calificaciones.py
@@ -5,7 +5,7 @@ def run():
     print(df.name)
     i = input("Hola! Para saber tu calificación escribe el número en el que apareces en la lista de arriba: ")
     print("Hola " + str(df.iloc[int(i),0]) +"!")
-    calificacion = df.iloc[int(i),1]*0.3 + df.iloc[int(i),2]*0.3 + df.iloc[int(i),3]*0.1 + df.iloc[int(i),4]*0.3
+    calificacion = df.iloc[int(i),1]*0.3 + df.iloc[int(i),2]*0.2 + df.iloc[int(i),3]*0.1 + df.iloc[int(i),4]*0.4
     print("Tu calificación es " + str(calificacion))
     
 
